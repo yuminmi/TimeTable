5c0791d30e08094f2208a50c373a143491ac1800
doyeon <doyun20445@gmail.com>
2025-05-07 14:29:14 +0900

    feat: Add createTimeTable

--- src/main/java/NotModified/TimeTable/controller/TimeTableApiController.java
+++ src/main/java/NotModified/TimeTable/controller/TimeTableApiController.java
@@ -2,6 +2,7 @@ package NotModified.TimeTable.controller;
 
 import NotModified.TimeTable.domain.Course;
 import NotModified.TimeTable.domain.TimeTable;
+import NotModified.TimeTable.dto.timeTable.TimeTableRequestDto;
 import NotModified.TimeTable.dto.timeTableWithCourse.TimeTableWithCourseRequestDto;
 import NotModified.TimeTable.service.CourseService;
 import NotModified.TimeTable.service.TimeTableDetailService;
@@ -20,6 +21,16 @@ public class TimeTableApiController {
     private final TimeTableService timeTableService;
     private final CourseService courseService;
     private final TimeTableDetailService timeTableDetailService;
+
+    @PostMapping("timetable/")
+    public ResponseEntity<?> createTimeTable(@RequestBody TimeTableRequestDto request) {
+        timeTableService.saveTimeTable(request);
+        return ResponseEntity.ok(Map.of(
+                "success", true,
+                "message", "timeTable 생성 성공"
+                )
+        );
+    }
 
     @PostMapping("/timetable/detail")
     public ResponseEntity<?> createTimeTableDetail(@RequestBody TimeTableWithCourseRequestDto request) {
