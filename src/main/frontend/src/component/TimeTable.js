2ed05ec360e49c64f654886056d091976f871bd4
yuminmi <minming0400@naver.com>
2025-05-07 14:08:47 +0900

    feat: 시간표에 과목 띄우기 구현

--- src/main/frontend/src/component/TimeTable.js
+++ src/main/frontend/src/component/TimeTable.js
@@ -1,6 +1,11 @@
 import styles from "./TimeTable.module.css";
 
-export default function TimeTable() {
+export default function TimeTable({timeItem}) {
+    function parseTimeToFloat(timeStr) {
+        const [h, m] = timeStr.split(":").map(str => parseInt(str, 10)); // 9:30 형식을 10진수로 h=9, m=30저장
+        return h + (m/60); // 9 + 30/60 = 9+0.5 => 시간 단위로 계산
+    }
+
     return (
         <div className={styles.tableBlock}>
             <div className={styles.tableHeader}>
@@ -21,15 +26,41 @@ export default function TimeTable() {
                       <div className={styles.timeCell}>
                         {rowIdx + 9 <= 12
                           ? rowIdx + 9
-                          : rowIdx + 9 > 24
-                          ? rowIdx - 15
                           : rowIdx + 9 - 12}
                       </div>
                       {[...Array(7)].map((_, colIdx) => (
                         <div className={styles.cell} key={`cell-${rowIdx}-${colIdx}`}></div>
                       ))}
                     </>
-                  ))}
+                ))}
+                {timeItem.map((item) =>
+                 item.times.map((time, index) => {
+                    const dayIndex = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].indexOf(time.day);
+                    const startTime = parseTimeToFloat(time.startTime);
+                    const endTime = parseTimeToFloat(time.endTime);
+                    const HEADER_HEIGHT = 50; // dayIndex 한 줄의 height
+                    const CELL_HEIGHT = 50; // cell 한 칸당 height
+                    const top = HEADER_HEIGHT + (startTime - 9) * CELL_HEIGHT; // 9시부터 시작이니까 0으로 시작
+                    const height = (endTime - startTime) * 50;
+                    return <div key={`${item.subject}-${index}`}
+                         className={styles.timeBlock}
+                         style={{
+                            top:`${top}px`,
+                            left:`${dayIndex*100+60}px`, // 앞에 시간 width는 60
+                            height:`${height}px`,
+                            width:'100px',
+                            position: 'absolute',
+                            backgroundColor:'lightblue',
+                            border:'1px solid #999',
+                            padding:'4px',
+                            boxSizing:'border-box',
+                            fontSize:'10px'
+                         }}
+                    >
+                    {console.log(item)}
+                        {item.subject}<br/>{item.instructor}<br/>{time.loca}
+                    </div>
+                 }))}
             </div>
 
         </div>
--- src/main/frontend/src/component/TimeTablePage.js
+++ src/main/frontend/src/component/TimeTablePage.js
@@ -5,9 +5,15 @@ import AddTimeTable from "./AddTimeTable";
 import {useState} from "react";
 
 export default function TimeTablePage() {
+const [timetable, setTimetable] = useState([]);
 const [isModalOpen, setIsModalOpen] = useState(false);
 const openModal = () => setIsModalOpen(true);
 const closeModal = () => setIsModalOpen(false);
+const handleAddItem = (newItem) => {
+    setTimetable([...timetable, newItem]);
+    setIsModalOpen(false);
+    alert(`${newItem.subject}가 등록되었습니다.`);
+}
 
     return (
         <div style={{display:"flex", padding:"210px"}}>
@@ -16,8 +22,8 @@ const closeModal = () => setIsModalOpen(false);
             <div>
             <button onClick={openModal}>+</button>
             </div>
-            <TimeTable />
-            <AddTimeTable isOpen={isModalOpen} closeModal={closeModal}/>
+            <TimeTable timeItem={timetable}/>
+            <AddTimeTable isOpen={isModalOpen} closeModal={closeModal} onAdd={handleAddItem}/>
         </div>
     );
 }
