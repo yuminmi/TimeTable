073c99e09122e3b04663a05a50cf41712306431b
yuminmi <minming0400@naver.com>
2025-05-09 15:14:18 +0900

    feat: dto연결 test

--- src/main/frontend/src/component/TimeTablePage.js
+++ src/main/frontend/src/component/TimeTablePage.js
@@ -25,19 +25,17 @@ export default function TimeTablePage() {
                 try {
                     const dayIndex = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"].indexOf(newItem.times[0].day);
                     const response = await axios.post('/api/timetable/detail', {
-                        "TimeTableWithCourseRequestDto": {
-                            "TimeTableDetailRequestDto": {
-                                weekday: dayIndex,
-                                location: newItem.times[0].loca,
-                                startTime: newItem.times[0].startTime,
-                                endTime: newItem.times[0].endTime
-                            },
-                            "CourseRequestDto": {
-                                userId: userName,
-                                title: newItem.subject,
-                                instructor: newItem.instructor,
-                                color: "#add8e6"
-                            }
+                        "TimeTableDetailRequestDto": {
+                            "weekday": dayIndex,
+                            "location": newItem.times[0].loca,
+                            "startTime": newItem.times[0].startTime,
+                            "endTime": newItem.times[0].endTime
+                        },
+                        "CourseRequestDto": {
+                            "userId": userName,
+                            "title": newItem.subject,
+                            "instructor": newItem.instructor,
+                            "color": "#add8e6"
                         }
                     });
                     console.log("서버 응답:", response.data);
