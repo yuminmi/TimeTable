a5efb37677ca0ea46fd1797ee913d7eee9572aeb
doyeon <doyun20445@gmail.com>
2025-05-13 21:05:52 +0900

    feat: Add update, delete

--- src/main/java/NotModified/TimeTable/controller/TimeTableApiController.java
+++ src/main/java/NotModified/TimeTable/controller/TimeTableApiController.java
@@ -4,7 +4,9 @@ import NotModified.TimeTable.domain.Course;
 import NotModified.TimeTable.domain.TimeTable;
 import NotModified.TimeTable.dto.timeTable.TimeTableRequestDto;
 import NotModified.TimeTable.dto.timeTable.TimeTableResponseDto;
+import NotModified.TimeTable.dto.timeTable.TimeTableUpdateDto;
 import NotModified.TimeTable.dto.timeTableWithCourse.TimeTableWithCourseRequestDto;
+import NotModified.TimeTable.dto.timeTableWithCourse.TimeTableWithCourseResponseDto;
 import NotModified.TimeTable.service.CourseService;
 import NotModified.TimeTable.service.TimeTableDetailService;
 import NotModified.TimeTable.service.TimeTableService;
@@ -43,7 +45,30 @@ public class TimeTableApiController {
                 )
         );
     }
-    
+
+    // 시간표 수정 : is_main 관리 - is_main 을 수정할 경우, 기존 is_main 을 false 로 바꾼 뒤 업데이트
+    @PutMapping("/timetable/{id}")
+    public ResponseEntity<?> updateTimeTable(@PathVariable("id") Long id,
+                                             @RequestBody TimeTableUpdateDto request) {
+        timeTableService.updateTimeTable(id, request);
+        return ResponseEntity.ok(Map.of(
+                "success", true,
+                "message", "timeTable 수정 성공"
+        ));
+    }
+
+    // 시간표 삭제
+    @DeleteMapping("/timetable/{id}")
+    public ResponseEntity<?> deleteTimeTable(@PathVariable("id") Long id) {
+        timeTableService.deleteTimeTable(id);
+        
+        return ResponseEntity.ok(Map.of(
+                "success", true,
+                "message", "timeTable 삭제 성공"
+        ));
+    }
+
+    // 세부 시간표 등록
     @PostMapping("/timetable/detail")
     public ResponseEntity<?> createTimeTableDetail(@RequestBody TimeTableWithCourseRequestDto request) {
         Long timeTableId = request.getTableDetailDto().getTimeTableId();
@@ -61,6 +86,43 @@ public class TimeTableApiController {
         return ResponseEntity.ok(Map.of(
                 "success", true,
                 "message", "timeTableDetail 생성 성공"
+        ));
+    }
+
+    // 세부 시간표 목록 조회
+    @GetMapping("/timetable/detail/{id}")
+    public ResponseEntity<?> getAllTimeTableDetail(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(Map.of(
+                        "success", true,
+                        "message", "세부 시간표 목록 조회 성공",
+                        "data", timeTableDetailService.findAllTimeTableDetails(id)
+                )
+        );
+    }
+
+    // 세부 시간표 수정 : course 정보 수정 주의 (title, instructor, color)
+    // TimeTableDetailUpdateDto, CourseUpdateDto
+    @PutMapping("/timetable/detail")
+    public ResponseEntity<?> updateTimeTableDetail(@RequestBody TimeTableWithCourseRequestDto request) {
+        timeTableDetailService.updateTimeTableDetail(request.getDetailUpdateDto());
+        courseService.updateCourse(request.getCourseUpdateDto());
+        return ResponseEntity.ok(Map.of(
+                "success", true,
+                "message", "timeTableDetail 수정 성공"
+        ));
+    }
+
+    // 세부 시간표 삭제 : 선 삭제 후, 해당 course_id를 참조하는 튜플이 없는 경우, course 도 삭제
+    @DeleteMapping("/timetable/detail/{id}")
+    public ResponseEntity<?> deleteTimeTableDetail(@PathVariable("id") Long id) {
+        Long courseId = timeTableDetailService.deleteTimeTableDetail(id);
+        // 해당 강좌를 참조하는 세부 시간표가 없으면 그 강좌도 함께 삭제
+        if(timeTableDetailService.findByCourseId(courseId) == 0) {
+            courseService.deleteCourse(courseId);
+        }
+        return ResponseEntity.ok(Map.of(
+                "success", true,
+                "message", "timeTableDetail 삭제 성공"
         ));
     }
 }
